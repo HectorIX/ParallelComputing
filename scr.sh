1bb5125598cd6126379f08baced27da613112368
HectorIX <giorgoschiotis@protonmail.com>
2017-02-23 15:35:03 +0000

    measurements

--- scr.sh
+++ scr.sh
@@ -2,9 +2,9 @@
 COUNT=2
 # bash until loop
 
-until [ $COUNT -gt 64 ]; do
+until [ $COUNT -gt 12 ]; do
 
-	srun -n $COUNT --mpi=pmi2 ./a.out 1 30000 >> 30k.txt  
+	srun -n $COUNT --mpi=pmi2 ./a.out 1 100000 >> 100k_part1.txt  
 
         let COUNT=COUNT+1
 done 
