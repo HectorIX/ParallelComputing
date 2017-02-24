d49d058fc2a8f0e6bb952e1f33ea764d328aa129
HectorIX <giorgoschiotis@protonmail.com>
2017-02-24 10:33:13 +0000

    optimal measurements

--- scr.sh
+++ scr.sh
@@ -2,9 +2,9 @@
 COUNT=2
 # bash until loop
 
-until [ $COUNT -gt 12 ]; do
+until [ $COUNT -gt 64 ]; do
 
-	srun -n $COUNT --mpi=pmi2 ./a.out 1 100000 >> 100k_part1.txt  
+	srun -n $COUNT --mpi=pmi2 ./a.out 1 100000 >> opt_100k.txt  
 
         let COUNT=COUNT+1
 done 
